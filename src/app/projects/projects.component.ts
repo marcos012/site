1278a4e1bb45f0d5a7e7b542b664e31e3203f5c5
marcos012 <marcospaulo.ad@gmail.com>
2018-12-25 15:20:41 -0200

    adiciona classe de css

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  templateUrl: './projects.component.html'
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
 
